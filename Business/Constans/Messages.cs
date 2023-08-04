d93647b5d71de95120fd9068d3595d30b3051d2b
huseyin.kardes <huseyin.kardes@BDH-5CG6023ZZ2>
2023-08-04 16:24:42 +0300

    Add project files.

---
+++ Business/Constans/Messages.cs
@@ -0,0 +1,21 @@
+﻿using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Constans
+{
+    public static class Messages
+    {
+        public static string ProductAdded = "Ürün Eklendi";
+        public static string ProductNameInvalid = "Ürün İsmi Geçersiz";
+        public static string MaintenanceTime = "Sistem Bakımda";
+        public static string ProductsListed = "Ürünler Listelendi";
+
+        public static string ProductCountOfCategoryError = "Bir kategoride en fazla 10 ürün olabilir";
+
+        public static string ProductNameAlreadyExists = "Bu isimde bir ürün zaten bulunuyor";
+
+        public static string CategoryLimitExceded = " Kategori limiti aşıldığı için yeni ürün eklenemiyor";
+    }
+}
